392393fa6db918d3ce39639bf7c40abb4386da58
rahul-53 <yrahul0620@gmail.com>
2021-11-03 23:20:17 +0530

    firebase auth completed

--- app/src/main/java/com/example/airbnbclone/MainActivity.kt
+++ app/src/main/java/com/example/airbnbclone/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.airbnbclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mAuth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
+
+        Handler().postDelayed({
+            if (user!=null){
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else{
+                val signInIntent  = Intent(this,LoginActivity::class.java)
+                startActivity(signInIntent)
+                finish()
+            }
+
+        },3000)
+
     }
 }
